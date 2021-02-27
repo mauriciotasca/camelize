02f925578cae5870a108d7baf326da4d1a835ef9
Mauricio Correa <mcorrea@avenuecode.com>
2021-02-27 00:47:27 -0300

    feat: Add fonts and initial css styling: * Add imports for Merienda One and Quicksand * Add CSS for positioning form in the center * Add logic to handle submit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { FormEvent, useRef } from "react";
+import "./App.css";
 
 function App() {
+  const searchInputRef = useRef<HTMLInputElement>(null);
+  const handleSearchSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(searchInputRef?.current?.value);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="camelize-wrapper">
+      <h2 className="title">camelize</h2>
+      <form className="form" noValidate onSubmit={handleSearchSubmit}>
+        <input ref={searchInputRef} className="search-input" type="text" />
+        <button type="submit" className="search-button">
+          Search
+        </button>
+      </form>
     </div>
   );
 }
